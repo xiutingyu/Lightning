4362a5881783be64c224fa369ed618b40e971f69
xiutingyu <yuxiuting@yahoo.com>
2015-09-04 08:16:30 -0700

    fhf

--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -1,16 +1,32 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Lightning extends PApplet {
+
 
 int startX=0;
 int startY=150;
 int endX=0;
 int endY=150;
-void setup()
+public void setup()
 {
   size(300,300);
   //strokeWeight(6);
   noLoop();
 
 }
-void draw()
+public void draw()
 {
 	stroke(((int)(Math.random()*256)),((int)(Math.random()*256)),((int)(Math.random()*256)));
 	
@@ -34,7 +50,7 @@ void draw()
 	}
 
 }
-void mousePressed()
+public void mousePressed()
 {
 	fill(100,100,100);
 rect(20,30,40,50);
@@ -45,3 +61,12 @@ endY=150;
 redraw();
 }
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Lightning" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
